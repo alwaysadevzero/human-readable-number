ca0442f2f597dd65c5f0af932d57b29093bc9120
alwaysadevzero <119855949+alwaysadevzero@users.noreply.github.com>
2023-02-09 21:21:11 +0400

    refactor: again

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 module.exports = function toReadable (number) {
     function get_unit(num) {
-        let number = num.toString(10);
         let units = {
             '0': 'zero', '1': 'one','2': 'two',
             '3': 'three','4': 'four','5': 'five',
@@ -10,41 +9,36 @@ module.exports = function toReadable (number) {
             '15': 'fifteen','16': 'sixteen','17': 'seventeen',
             '18': 'eighteen', '19': 'nineteen'
         }
-        return units[number]
+        return units[num.toString(10)]
     }
     function get_teens(num) {
-        let number = num.toString(10);
         let teens = {
             '20': 'twenty','30': 'thirty',
             '40': 'forty','50': 'fifty',
             '60': 'sixty','70': 'seventy',
             '80': 'eighty','90': 'ninety',
         }
-        return teens[number]
+        return teens[num.toString(10)]
     }
     function get_hundred(num){
         let first_digit = Math.floor(num/100).toString(10);
         return get_unit(first_digit) + ' hundred'
     }
     function convert_teens(num) {
-        num < 20 ? return get_unit(num)
+        if num < 20 return get_unit(num)
 
         let teens = Math.floor(num/10)*10;
         let unit = num % 10;
-        (unit) ? return get_teens(teens) + ' ' + get_unit(unit) : return get_teens(teens)
+        return unit ? get_teens(teens) + ' ' + get_unit(unit) : get_teens(teens)
      }
     function convert_hundred(num) {
         let hundred = Math.floor(num/100)*100;
         let teens = num % 100;
-        (teens) ? return get_hundred(hundred) + ' ' + convert_teens(teens) : return get_hundred(hundred)
+        return teens ? get_hundred(hundred) + ' ' + convert_teens(teens) : get_hundred(hundred)
     }
     function converter(num) {
-        if (num < 100) {
-            return convert_teens(num)
-        }
-        else if (num < 1000) {
-            return convert_hundred(num)
-        }
+        if (num < 100) return convert_teens(num)
+        if (num < 1000) return convert_hundred(num)
     }
     return converter(number)
 }
