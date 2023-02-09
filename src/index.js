eb7554a8e3391a7382a9491131691d7ef393afed
alwaysadevzero <119855949+alwaysadevzero@users.noreply.github.com>
2023-02-09 21:01:08 +0400

    refactor: change code

--- src/index.js
+++ src/index.js
@@ -1,44 +1,24 @@
 module.exports = function toReadable (number) {
-  
     function get_unit(num) {
         let number = num.toString(10);
-
         let units = {
-        '0': 'zero', 
-        '1': 'one',
-        '2': 'two',
-        '3': 'three',
-        '4': 'four',
-        '5': 'five',
-        '6': 'six',
-        '7': 'seven',
-        '8': 'eight',
-        '9': 'nine',
-        '10': 'ten',
-        '11': 'eleven',
-        '12': 'twelve',
-        '13': 'thirteen',
-        '14': 'fourteen',
-        '15': 'fifteen',
-        '16': 'sixteen',
-        '17': 'seventeen',
-        '18': 'eighteen',
-        '19': 'nineteen',
+            '0': 'zero', '1': 'one','2': 'two',
+            '3': 'three','4': 'four','5': 'five',
+            '6': 'six', '7': 'seven', '8': 'eight',
+            '9': 'nine','10': 'ten','11': 'eleven',
+            '12': 'twelve','13': 'thirteen','14': 'fourteen',
+            '15': 'fifteen','16': 'sixteen','17': 'seventeen',
+            '18': 'eighteen', '19': 'nineteen'
         }
         return units[number]
     }
     function get_teens(num) {
         let number = num.toString(10);
-        
         let teens = {
-            '20': 'twenty',
-            '30': 'thirty',
-            '40': 'forty',
-            '50': 'fifty',
-            '60': 'sixty',
-            '70': 'seventy',
-            '80': 'eighty',
-            '90': 'ninety',
+            '20': 'twenty','30': 'thirty',
+            '40': 'forty','50': 'fifty',
+            '60': 'sixty','70': 'seventy',
+            '80': 'eighty','90': 'ninety',
         }
         return teens[number]
     }
@@ -46,29 +26,20 @@ module.exports = function toReadable (number) {
         let first_digit = Math.floor(num/100).toString(10);
         return get_unit(first_digit) + ' hundred'
     }
+    function convert_teens(num) {
+        num < 20 ? return get_unit(num)
+
+        let teens = Math.floor(num/10)*10;
+        let unit = num % 10;
+        (unit) ? return get_teens(teens) + ' ' + get_unit(unit) : return get_teens(teens)
+     }
+    function convert_hundred(num) {
+        let hundred = Math.floor(num/100)*100;
+        let teens = num % 100;
+        (teens) ? return get_hundred(hundred) + ' ' + convert_teens(teens) : return get_hundred(hundred)
+    }
     function converter(num) {
-        function convert_teens(num) {
-            if (num < 20) {
-                return get_unit(num)
-            }
-            let teens = Math.floor(num/10)*10;
-            let unit = num % 10;
-
-            if (unit){
-                return get_teens(teens) + ' ' + get_unit(unit)
-            }
-            return get_teens(teens)
-        }
-        function convert_hundred(num) {
-            let hundred = Math.floor(num/100)*100;
-            let teens = num % 100;
-
-            if (teens){
-                return get_hundred(hundred) + ' ' + convert_teens(teens)
-            }
-            return get_hundred(hundred)
-        }
-        else if (num < 100) {
+        if (num < 100) {
             return convert_teens(num)
         }
         else if (num < 1000) {
